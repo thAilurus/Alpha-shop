f2a2c93ef9ba3cc3bde51f681833573e1a9ad371
Joanna <joanna211119@gmail.com>
2022-11-17 20:17:15 +0800

    Add CSS style on mainContainer

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Register } from './register/register'
+import styles from './main.module.scss'
 
 export function Main () {
   return (
-    <Register/>
+    // 之後main.scss要對mainContainer做style的調整
+    <div className={styles.mainContainer}> 
+      <Register/>
+    </div>
   )
 }
