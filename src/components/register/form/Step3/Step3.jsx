bc21a9ed94f3a05afb2922b8b43aaee2c0be8ce2
Joanna <joanna211119@gmail.com>
2022-11-18 18:08:46 +0800

    Add basic CSS styles in Step3.jsx

--- src/components/register/form/Step3/Step3.jsx
+++ src/components/register/form/Step3/Step3.jsx
@@ -4,31 +4,37 @@ import styles from './Step3.module.scss'
 export function Step3 () {
   return (
     <>
-      <form class="col col-12" data-phase="credit-card">
-        <h3 class="form-title">付款資訊</h3>
-        <section class="form-body col col-12">
-          <div class="col col-12">
-            <div class="input-group input-w-lg-4 input-w-sm-full">
-              <div class="input-label">持卡人姓名</div>
+      <form className={styles.step3Credit} data-phase="credit-card">
+        <h3 className={styles.title}>付款資訊</h3>
+        
+        <section className={styles.formBody}>
+
+          <div className={styles.col12}>
+            <div className={styles.inputGroup}>
+              <div className={styles.inputLabel}>持卡人姓名</div>
               <input type="text" placeholder="John Doe" />
             </div>
           </div>
-          <div class="col col-12">
-            <div class="input-group input-w-lg-4 input-w-sm-full">
-              <div class="input-label">卡號</div>
+
+          <div className={styles.col12}>
+            <div className={styles.inputGroup}>
+              <div className={styles.inputLabel}>卡號</div>
               <input type="text" placeholder="1111 2222 3333 4444" />
             </div>
           </div>
-          <div class="col col-12">
-            <div class="input-group input-w-lg-3 input-w-sm-s3">
-              <div class="input-label">有效期限</div>
+
+          <div className={styles.col12}>
+            <div className={styles.inputGroup}>
+              <div className={styles.inputLabel}>有效期限</div>
               <input type="text" placeholder="MM/YY" />
             </div>
-            <div class="input-group input-w-lg-3 input-w-sm-s3">
-              <div class="input-label">CVC / CCV</div>
+
+            <div className={styles.inputGroup}>
+              <div className={styles.inputLabel}>CVC / CCV</div>
               <input type="text" placeholder="123" />
             </div>
           </div>
+
         </section>
       </form>
     </>
