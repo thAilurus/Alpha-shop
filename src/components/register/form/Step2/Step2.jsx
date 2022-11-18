41b6f59be0451b7ee3a4781445612ae632c65e62
Joanna <joanna211119@gmail.com>
2022-11-18 17:20:35 +0800

    Add Basic CSS styles in Step2.jsx

--- src/components/register/form/Step2/Step2.jsx
+++ src/components/register/form/Step2/Step2.jsx
@@ -5,31 +5,49 @@ import styles from './Step2.module.scss'
 export function Step2 () {
   return (
     <>
-      <form class="col col-12" data-phase="shipping">
-        <h3 class="form-title">運送方式</h3>
-        <section class="form-body col col-12">
-          <label class="radio-group col col-12" data-price="0">
-            <input id="shipping-standard" type="radio" name="shipping" checked />
-            <div class="radio-info">
-              <div class="col col-12">
-                <div class="text">標準運送</div>
-                <div class="price"></div>
+      <form className={styles.step2Shipping} data-phase="shipping">
+        <h3 className={styles.title}>運送方式</h3>
+
+        <section className={styles.formBody}>
+
+          <label className={styles.radioGroup} data-price="0">
+            <input className={styles.shippingStandard}
+            id="shipping-standard"
+            type="radio" 
+            name="shipping" 
+            checked 
+            />
+
+            <div className={styles.radioInfo}>
+              <div className={styles.col12}>
+                <div className={styles.shippingText}>標準運送</div>
+                <div className={styles.shippingPrice}></div>
               </div>
-              <div class="period col col-12">約 3~7 個工作天</div>
+              <div className={styles.shippingPeriod}>約 3~7 個工作天</div>
             </div>
-            <div class="radio-box-border"></div>
+
+            <div className={styles.radioBoxBorder}></div>
+
           </label>
-          <label class="radio-group col col-12" data-price="500">
-            <input id="shipping-dhl" type="radio" name="shipping" />
-            <div class="radio-info">
-              <div class="col col-12">
-                <div class="text">DHL 貨運</div>
-                <div class="price"></div>
+
+          <label className={styles.radioGroup} data-price="500">
+            <input className={styles.shippingStandard}
+            id="shipping-dhl" 
+            type="radio" 
+            name="shipping" 
+            />
+
+            <div className={styles.radioInfo}>
+              <div className={styles.col12}>
+                <div className={styles.shippingText}>DHL 貨運</div>
+                <div className={styles.shippingPrice}></div>
               </div>
-              <div class="period col col-12">48 小時內送達</div>
+              <div className={styles.shippingPeriod}>48 小時內送達</div>
             </div>
-            <div class="radio-box-border"></div>
+
+            <div className={styles.radioBoxBorder}></div>
           </label>
+
         </section>
       </form>
     </>
