ac2c15ef12887a2b1d9cbd72258953656a4bf3a6
Joanna <joanna211119@gmail.com>
2022-11-19 16:40:05 +0800

    Remove HTML

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,71 +6,12 @@ import { ReactComponent as Moon } from '../icons/moon.svg'
 import { ReactComponent as Sun } from '../icons/sun.svg'
 import { ReactComponent as Logo } from '../icons/logo.svg'
 
+//import styles from './header.module.scss'
+
 
 export function Header () {
   return (
     <>
-      <header class="site-header">
-        <div class="header-container mx-auto">
-          
-          <input id="navbar-toggle" class="navbar-toggle" type="checkbox" />
-          <label for="navbar-toggle" class="burger-container">
-            <svg class="icon-toggle cursor-point">
-              {/*<use xlink:href="#svg-icon-toggle"></use>*/}
-            </svg>
-          </label>
-          
-          <nav class="navbar-menu">
-            <ul class="nav-list site-menu-list mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">男款</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">女款</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">最新消息</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">客製商品</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">聯絡我們</a>
-              </li>
-            </ul>
-            <ul class="nav-list site-action-list">
-
-              <li class="nav-item">
-                <svg class="nav-icon cursor-point">
-                  {/*<use xlink:href="#svg-icon-search"></use>*/}
-                </svg>
-              </li>
-
-              <li class="nav-item">
-                <svg class="nav-icon cursor-point">
-                  {/*<use xlink:href="#svg-icon-cart"></use>*/}
-                </svg>
-              </li>
-              <li id="theme-toggle" class="nav-item">
-
-                <svg class="nav-icon cursor-point">
-                  {/*<use xlink:href="#svg-icon-moon"></use>*/}
-                </svg>
-                <svg class="nav-icon cursor-point">
-                  {/*<use xlink:href="#svg-icon-sun"></use>*/}
-                </svg>
-              </li>
-            </ul>
-          </nav>
-
-          <a class="header-logo-container" href="#">
-            <svg class="icon-logo cursor-point">
-             {/*} <use xlink:href="#svg-icon-logo"></use>*/}
-            </svg>
-          </a>
-        </div>
-      </header>
-
-    </>
+    </>            
   )
 }
