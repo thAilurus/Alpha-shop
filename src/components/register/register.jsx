bc493dabeb649b21f5cc2b595434dc9297b6ae79
Joanna <joanna211119@gmail.com>
2022-11-19 15:59:22 +0800

    Delete function Step1-Step3 and add function From in

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { StepProgress } from './StepProgress/StepProgress'
-import { Step1 } from './form/Step1/Step1'
-import { Step2 } from './form/Step2/Step2'
-import { Step3 } from './form/Step3/Step3'
+import { Form } from './form/Form'
+
 import { ProgressControl } from './ProgressControl/ProgressControl'
 
 export function Register () {
   return (
     <>
       <StepProgress/>
-      <Step1/>
-      <Step2/>
-      <Step3/>
+      <Form/>
       <ProgressControl/>
     </>
   )
