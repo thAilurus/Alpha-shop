477578bbfcb2d13f70d331bd92e3a98ff5fe5838
Joanna <joanna211119@gmail.com>
2022-11-20 17:24:21 +0800

    Adjust registerContainer, progressContainer and formContainer

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,6 +7,7 @@ export function Main () {
     // 之後main.scss要對mainContainer做style的調整
     <div className={styles.mainContainer}> 
       <Register/>
+      {/*<Cart/>*/}
     </div>
   )
 }
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -4,17 +4,17 @@ import { Form } from './form/Form'
 import { ProgressControl } from './ProgressControl/ProgressControl'
 import styles from './register.module.scss'
 
-export function Register () {
+export function Register() {
   return (
     <>
       <section className={styles.registerContainer} data-phase="1" data-total-price="0">
         <h2 className={styles.registerTitle}>結帳</h2>
+        <StepProgress />
+        <Form />
 
-        <StepProgress/>
-        <Form/>
-        <ProgressControl/>
-        
-      </section>  
+      </section>
+      <ProgressControl />
+      
     </>
   )
 }
