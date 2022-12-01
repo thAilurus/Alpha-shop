99e04a9356ac98993deb3c14afad54b3d22ed1a2
Joanna <joanna211119@gmail.com>
2022-12-01 17:28:38 +0800

    Import CSS styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,48 +7,49 @@ import { ReactComponent as Sun } from '../icons/sun.svg'
 import { ReactComponent as Logo } from '../icons/logo.svg'
 
 //import styles from './header.module.scss'
-
+import styles from './ header.module.scss'
 
 export function Header () {
   return (
     <>
-      <header class="site-header">
-        <div class="header-container mx-auto">
+      <header className={styles.siteHeader}>
+        <div className={styles.headerContainer}>
+
           {/*<!-- navbar-toggle -->*/}
-          <input id="navbar-toggle" class="navbar-toggle" type="checkbox" />
-          <label for="navbar-toggle" class="burger-container">
+          <input id="navbar-toggle" className={styles.navbarToggle} type="checkbox" />
+          <label for="navbar-toggle" className={styles.burgerContainer}>
             <Toggle/>
           </label>
 
           {/* <!-- navbar-menu -->*/}
-          <nav class="navbar-menu">
-            <ul class="nav-list site-menu-list mr-auto">
-              <li class="nav-item">
+          <nav className={styles.navbarMenu}>
+            <ul className={styles.navListMenu}>
+              <li className={styles.navItem}>
                 <a class="nav-link" href="#">男款</a>
               </li>
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <a class="nav-link" href="#">女款</a>
               </li>
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <a class="nav-link" href="#">最新消息</a>
               </li>
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <a class="nav-link" href="#">客製商品</a>
               </li>
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <a class="nav-link" href="#">聯絡我們</a>
               </li>
             </ul>
-            <ul class="nav-list site-action-list">
+            <ul className={styles.navListAction}>
               {/*<!-- search -->*/}
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <Search/>
               </li>
               {/*<!-- cart -->*/}
-              <li class="nav-item">
+              <li className={styles.navItem}>
                 <Cart/>
               </li>
-              <li id="theme-toggle" class="nav-item">
+              <li id="theme-toggle" className={styles.navItem}>
                 {/* <!-- moon --> */}
                 <Moon/>
                 {/*<!-- sun -->*/}
@@ -58,9 +59,10 @@ export function Header () {
           </nav>
 
           {/*<!-- logo -->*/}
-          <a class="header-logo-container" href="#">
+          <a className={styles.logoContainer} href="#">
             <Logo/>
           </a>
+          
         </div>
       </header>
     </>            
