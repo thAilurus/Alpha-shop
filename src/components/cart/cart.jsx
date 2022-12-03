fd571fd8b8cefec1118abadad25de80511aa3044
Joanna <joanna211119@gmail.com>
2022-12-03 17:40:33 +0800

    Import css styles

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -10,26 +10,26 @@ import styles from './cart.module.scss'
 export function Cart() {
   return (
     <>
-      <section class="cart-container col col-lg-5 col-sm-12">
-        <h3 class="cart-title">購物籃</h3>
+      <section className={styles.cartContainer}>
+        <h3 className={styles.cartTitle}>購物籃</h3>
 
-        <section class="product-list col col-12" data-total-price="0">
-          <div class="product-container col col-12" data-count="0" data-price="3999">
-            <img class="img-container" src="./public/images/product-1.jpg" />
-            <div class="product-info">
-              <div class="product-name">破壞補丁修身牛仔褲</div>
-              <div class="product-control-container">
-                <div class="product-control">
-                  <Minus />
-                  <span class="product-count"></span>
-                  <Plus />
+        <section className={styles.productList} data-total-price="0">
+          <div className={styles.productContainer} data-count="0" data-price="3999">
+            <img className={styles.imgContainer} alt='product1'  src="./public/images/product-1.jpg" />
+            <div className={styles.productInfo}>
+              <div className={styles.productName}>破壞補丁修身牛仔褲</div>
+              <div className={styles.productControlContainer}>
+                <div className={styles.productControl}>
+                  <Minus className={styles.minus} />
+                  <span className={styles.productCount}></span>
+                  <Plus className={styles.plus} />
                 </div>
               </div>
-              <div class="price"></div>
+              <div className={styles.price}></div>
             </div>
           </div>
           <div class="product-container col col-12" data-count="0" data-price="1299">
-            <img class="img-container" src="./public/images/product-2.jpg" />
+            <img class="img-container" alt='product2' src="./public/images/product-2.jpg" />
             <div class="product-info">
               <div class="product-name">刷色直筒牛仔褲</div>
               <div class="product-control-container">
@@ -45,13 +45,13 @@ export function Cart() {
           </div>
         </section>
 
-        <section class="cart-info shipping col col-12">
-          <div class="text">運費</div>
-          <div class="price"></div>
+        <section className={styles.cartShipping}>
+          <div className={styles.text}>運費</div>
+          <div className={styles.price}></div>
         </section>
-        <section class="cart-info total col col-12">
-          <div class="text">小計</div>
-          <div class="price"></div>
+        <section className={styles.cartTotal} >
+          <div className={styles.text}>小計</div>
+          <div className={styles.price}></div>
         </section>
       </section>
 
