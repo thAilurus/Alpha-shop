623889a4dd29eb86e71915b8434b08f3847337ff
Joanna <joanna211119@gmail.com>
2022-12-04 14:31:01 +0800

    Import SVG files

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
+import { ReactComponent as Logo } from '../icons/logo.svg'
+import { ReactComponent as FB } from '../icons/facebook.svg'
+import { ReactComponent as IG } from '../icons/instagram.svg'
+import { ReactComponent as Whatsapp } from '../icons/whatsapp.svg'
 
 
-export function Footer () {
+export function Footer() {
   return (
     <>
-    
+      <footer class="site-footer">
+        <div class="footer-container">
+          <div class="footer-logo-container">
+            <svg class="icon-logo">
+              <use xlink:href="#svg-icon-logo"></use>
+            </svg>
+          </div>
+          <section class="footer-section">
+            <h2 class="section-title">客戶服務</h2>
+            <div class="section-content">
+              <a class="page-link" href="#">運送說明</a>
+              <a class="page-link" href="#">退換貨相關</a>
+              <a class="page-link" href="#">付款資訊</a>
+              <a class="page-link" href="#">FAQ</a>
+            </div>
+          </section>
+          <section class="footer-section">
+            <h2 class="section-title">關於我們</h2>
+            <div class="section-content">
+              <a class="page-link" href="#">品牌故事</a>
+              <a class="page-link" href="#">媒體聯繫</a>
+              <a class="page-link" href="#">Press kit</a>
+            </div>
+          </section>
+          <section class="footer-section">
+            <h2 class="section-title">資訊</h2>
+            <div class="section-content">
+              <a class="page-link" href="#">隱私權政策</a>
+              <a class="page-link" href="#">Cookie</a>
+              <a class="page-link" href="#">GDPR</a>
+            </div>
+          </section>
+          <section class="footer-section">
+            <h2 class="section-title">追蹤 ALPHA Shop</h2>
+            <div class="section-content">
+              <div class="tel-info">+886 02123-45678</div>
+              <div class="social-icon-group">
+                <svg class="social-icon cursor-point">
+                  <use xlink:href="#svg-icon-facebook"></use>
+                </svg>
+                <svg class="social-icon cursor-point">
+                  <use xlink:href="#svg-icon-instagram"></use>
+                </svg>
+                <svg class="social-icon cursor-point">
+                  <use xlink:href="#svg-icon-whatsapp"></use>
+                </svg>
+              </div>
+            </div>
+          </section>
+        </div>
+      </footer>
     </>
   )
 }
