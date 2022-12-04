cecad919a2019ae2917cc7d56aef22bd54b32a7e
Joanna <joanna211119@gmail.com>
2022-12-04 15:23:30 +0800

    Import css styles

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,48 +4,50 @@ import { ReactComponent as FB } from '../icons/facebook.svg'
 import { ReactComponent as IG } from '../icons/instagram.svg'
 import { ReactComponent as Whatsapp } from '../icons/whatsapp.svg'
 
+import styles from './footer.module.scss'
+
 
 export function Footer() {
   return (
     <>
-      <footer class="site-footer">
-        <div class="footer-container">
-          <div class="footer-logo-container">
-            <Logo/>
+      <footer className={styles.siteFooter}>
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLogoContainer}>
+            <Logo className={styles.footerLogo} />
           </div>
-          <section class="footer-section">
-            <h2 class="section-title">客戶服務</h2>
-            <div class="section-content">
-              <a class="page-link" href="#">運送說明</a>
-              <a class="page-link" href="#">退換貨相關</a>
-              <a class="page-link" href="#">付款資訊</a>
-              <a class="page-link" href="#">FAQ</a>
+          <section className={styles.footerSection}>
+            <h2 className={styles.sectionTitle}>客戶服務</h2>
+            <div className={styles.sectionContent}>
+              <a className={styles.pageLink} href="#">運送說明</a>
+              <a className={styles.pageLink} href="#">退換貨相關</a>
+              <a className={styles.pageLink} href="#">付款資訊</a>
+              <a className={styles.pageLink} href="#">FAQ</a>
             </div>
           </section>
-          <section class="footer-section">
-            <h2 class="section-title">關於我們</h2>
-            <div class="section-content">
-              <a class="page-link" href="#">品牌故事</a>
-              <a class="page-link" href="#">媒體聯繫</a>
-              <a class="page-link" href="#">Press kit</a>
+          <section className={styles.footerSection}>
+            <h2 className={styles.sectionTitle}>關於我們</h2>
+            <div className={styles.sectionContent}>
+              <a className={styles.pageLink} href="#">品牌故事</a>
+              <a className={styles.pageLink} href="#">媒體聯繫</a>
+              <a className={styles.pageLink} href="#">Press kit</a>
             </div>
           </section>
-          <section class="footer-section">
-            <h2 class="section-title">資訊</h2>
-            <div class="section-content">
-              <a class="page-link" href="#">隱私權政策</a>
-              <a class="page-link" href="#">Cookie</a>
-              <a class="page-link" href="#">GDPR</a>
+          <section className={styles.footerSection}>
+            <h2 className={styles.sectionTitle}>資訊</h2>
+            <div className={styles.sectionContent}>
+              <a className={styles.pageLink} href="#">隱私權政策</a>
+              <a className={styles.pageLink} href="#">Cookie</a>
+              <a className={styles.pageLink} href="#">GDPR</a>
             </div>
           </section>
-          <section class="footer-section">
-            <h2 class="section-title">追蹤 ALPHA Shop</h2>
-            <div class="section-content">
-              <div class="tel-info">+886 02123-45678</div>
-              <div class="social-icon-group">
-                <FB/>
-                <IG/>
-                <Whatsapp/>
+          <section className={styles.footerSection}>
+            <h2 className={styles.sectionTitle}>追蹤 ALPHA Shop</h2>
+            <div className={styles.sectionContent}>
+              <div className={styles.telInfo}>+886 02123-45678</div>
+              <div className={styles.socialIconGroup}>
+                <FB className={styles.socialIcon} />
+                <IG className={styles.socialIcon} />
+                <Whatsapp className={styles.socialIcon} />
               </div>
             </div>
           </section>
