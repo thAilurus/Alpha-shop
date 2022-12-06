ac8df087c7d0fc3167d37adc2e6d0af6fc4a7d07
Joanna <joanna211119@gmail.com>
2022-12-06 18:58:05 +0800

    Use map() to render Cart component

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -19,79 +19,29 @@ function CartInfo({ name, text, price }) {
 export function Cart() {
   const listItems = items.map((item) => {
 
-      <div className={styles.productContainer} data-count="0" data-price="3999">
-        <img className={styles.imgContainer} src="./images/product-1.jpg" alt='product1' />
+      <div className={styles.productContainer} key={item.id}>
+        <img className={styles.imgContainer} src={item.img} alt={item.name} />
         <div className={styles.productInfo}>
-          <div className={styles.productName}>破壞補丁修身牛仔褲</div>
+          <div className={styles.productName}>{item.name}</div>
           <div className={styles.productControlContainer}>
             <div className={styles.productControl}>
               <Minus className={styles.minus} />
-              <span className={styles.productCount}>0</span>
+              <span className={styles.productCount}>{item.quantity}</span>
               <Plus className={styles.plus} />
             </div>
           </div>
-          <div className={styles.price}>$3999</div>
+          <div className={styles.price}>{item.price}</div>
         </div>
       </div>
-      <div className={styles.productContainer} data-count="0" data-price="1299">
-        <img className={styles.imgContainer} src="./images/product-2.jpg" alt='product2' />
-        <div className={styles.productInfo}>
-          <div className={styles.productName}>刷色直筒牛仔褲</div>
-          <div className={styles.productControlContainer}>
-            <div className={styles.productControl}>
-              <Minus className={styles.minus} />
-              <span className={styles.productCount}>0</span>
-              <Plus className={styles.plus} />
-            </div>
-          </div>
-          <div className={styles.price}>$1299</div>
-
-        </div>
-      </div>
-
   })
 
 return (
   <>
     <section className={styles.cartContainer}>
       <h3 className={styles.cartTitle}>購物籃</h3>
-
-      <section className={styles.productList} data-total-price="0">
-        <div className={styles.productContainer} data-count="0" data-price="3999">
-          <img className={styles.imgContainer} src="./images/product-1.jpg" alt='product1' />
-          <div className={styles.productInfo}>
-            <div className={styles.productName}>破壞補丁修身牛仔褲</div>
-            <div className={styles.productControlContainer}>
-              <div className={styles.productControl}>
-                <Minus className={styles.minus} />
-                <span className={styles.productCount}>0</span>
-                <Plus className={styles.plus} />
-              </div>
-            </div>
-            <div className={styles.price}>$3999</div>
-          </div>
-        </div>
-        <div className={styles.productContainer} data-count="0" data-price="1299">
-          <img className={styles.imgContainer} src="./images/product-2.jpg" alt='product2' />
-          <div className={styles.productInfo}>
-            <div className={styles.productName}>刷色直筒牛仔褲</div>
-            <div className={styles.productControlContainer}>
-              <div className={styles.productControl}>
-                <Minus className={styles.minus} />
-                <span className={styles.productCount}>0</span>
-                <Plus className={styles.plus} />
-              </div>
-            </div>
-            <div className={styles.price}>$1299</div>
-
-          </div>
-        </div>
-      </section>
-
+      <section className={styles.productList}>{listItems} </section>
       <CartInfo name="shipping" text="運費" price="免費" />
       <CartInfo name="total" text="小計" price="$0" />
-
-
     </section>
 
   </>
